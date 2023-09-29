b0b6bcb68f32cff48f663b8c6eee292135aec7d2
LandonTopy <landontopy@gmail.com>
2023-09-29 09:12:07 -0400

    9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int first = 0;
+        int second = 0;
+
+        System.out.println("What is the first number?");
+        if (scan.hasNextInt()) {
+            first = scan.nextInt();
+        }
+        else {
+            System.out.println("The value you entered was not valid");
+            System.exit(0);
+        }
+
+        System.out.println("What is the second number?");
+        if (scan.hasNextInt()) {
+            second = scan.nextInt();
+        }
+        else {
+            System.out.println("The value you entered was not valid");
+            System.exit(0);
+        }
+        if (first > second) {
+            System.out.println("" + first + " is greater than " + second);
+        } else if (second > first) {
+            System.out.println("" + second + " is greater than " + first);
+        } else {
+            System.out.println("" + first + " is equal to " + second);
+        }
     }
 }
